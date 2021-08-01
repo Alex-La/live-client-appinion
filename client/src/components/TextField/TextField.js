80aaff8322b5143717cff35992e33cecd22c6d2f
Alex-La <55190911+Alex-La@users.noreply.github.com>
2021-08-02 01:41:48 +0300

    Update text field

--- client/src/components/TextField/TextField.js
+++ client/src/components/TextField/TextField.js
@@ -1,5 +1,13 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import "./textField.css";
+
+const defaultStyle = { left: 7, top: 16, color: "#9ba0b9", fontSize: 14 };
+const focusStyle = {
+  left: 7,
+  fontSize: 12,
+  top: -8,
+  color: "var(--color)",
+};
 
 const TextField = ({
   label,
@@ -10,17 +18,34 @@ const TextField = ({
   style,
   placeholder,
 }) => {
+  const inputRef = useRef();
+  const [labelStyle, setLableStyle] = useState(defaultStyle);
+
+  const handleFocus = () => setLableStyle(focusStyle);
+  const handleBlur = () =>
+    setLableStyle(
+      value ? { ...labelStyle, color: defaultStyle.color } : defaultStyle
+    );
+
+  const focus = () => inputRef.current && inputRef.current.focus();
+
   return (
     <div className="text-field-root" style={style}>
       <input
-        id="text-field"
+        ref={inputRef}
+        onFocus={handleFocus}
+        onBlur={handleBlur}
         name={name}
         type={type}
         value={value}
         onChange={onChange}
         placeholder={placeholder}
       />
-      {label && <label htmlFor="text-field">{label}</label>}
+      {label && (
+        <label onClick={focus} style={labelStyle}>
+          {label}
+        </label>
+      )}
     </div>
   );
 };
